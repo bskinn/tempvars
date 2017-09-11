e748d77b9c435c025fda06f1d9c94af028d88edd
Brian Skinn <brian.skinn@gmail.com>
2017-09-10 23:29:58 -0400

    ADMIN: Initial setup.py, CHANGELOG.md
    setup.py should be pretty good. CHANGELOG is of course empty.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from os import environ
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='tempvars',
+    version='0.0',
+    provides=['tempvars'],
+    install_requires=['attrs'],
+    packages=['tempvars'],
+    url='https://www.github.com/bskinn/tempvars',
+    license='MIT License',
+    author='Brian Skinn',
+    author_email='bskinn@alum.mit.edu',
+    description='Context Manager for Handling Temporary Variables',
+    long_description=readme(),
+    classifiers=['License :: OSI Approved :: MIT License',
+                 'Natural Language :: English',
+                 'Environment :: Console',
+                 'Intended Audience :: Science/Research',
+		 'Intended Audience :: Developers',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3 :: Only',
+                 'Topic :: Software Development',
+                 'Development Status :: 2 - Pre-Alpha']
+    )
