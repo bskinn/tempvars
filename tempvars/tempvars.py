d3e5ddd65c27c365e0e916287b470791675912d6
Brian Skinn <brian.skinn@gmail.com>
2017-09-11 18:58:57 -0400

    CLEANUP: Comments and vars list validation
    * Standardized the `TypeError` raised during validation of `tempvars`, `starts`, and `ends`
    * Misc. comment cleanup

--- tempvars/tempvars.py
+++ tempvars/tempvars.py
@@ -29,25 +29,31 @@ class TempVars(object):
     @starts.validator
     @ends.validator
     def must_be_None_or_iterable_of_string(self, at, val):
+        # Standard error for failure return
+        te = TypeError("'{0}' must be a list of str".format(at.name))
+        
         if val is None:
             return
 
         if type(val) != list:
-            raise TypeError("'{0}' must be a list of str".format(at.name))
+            raise te
 
         for _ in val:
             if type(_) != str:
-                raise TypeError('Variable names must be strings')
+                raise te
         else:
             # Reached the end of the list, so everything's fine
             return
 
-        raise TypeError('Variable names must be strings')
+        # Fall-through to this point means it's something other than
+        # a list of strings
+        raise te
 
     # Flag for whether to restore the prior namespace contents
     restore = attr.ib(default=True, validator=attr.validators.instance_of(bool))
 
-    # Namespace to manipulate defaults to that which instantiated the class.
+    # Namespace for temp variable management. Defaults to the locals() of the
+    # scope at which the TempVars instance was created.
     # If a different namespace is desired for some reason, it can be passed here
     ns = attr.ib()
 
@@ -63,12 +69,15 @@ class TempVars(object):
         if not (type(val) == dict and all(map(lambda s: type(s) == str, val.keys()))):
             raise ValueError("'ns' must be a namespace dict")
 
-    # Internal vars, not set via the attrs __init__
+    ### Internal vars, not set via the attrs __init__
+    # Bucket for preserving variables temporarily removed from 
+    # the namespace
     stored_nsvars = attr.ib(init=False, default={})
 
+
     def __enter__(self):
-        # Search the namespace for anything matching the starts_with or
-        # ends_with
+        # Search the namespace for anything matching the .starts or
+        # .ends patterns
         for k in self.ns.keys():
             if self.starts is not None:
                 for sw in self.starts:
@@ -91,8 +100,7 @@ class TempVars(object):
                 # No var found in namespace, do nothing
                 pass
 
-        # Return this class so that users can inspect it if they
-        # need to for some reason
+        # Return instance so that users can inspect it if needed
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -100,10 +108,9 @@ class TempVars(object):
         list(self.ns.pop(_) for _ in self.tempvars if _ in self.ns)
 
         # If restore is set, then repopulate the namespace with
-        # the pre-existing values.
-        # Otherwise, just do nothing.
+        # the pre-existing values.  Otherwise, do nothing.
         if self.restore:
             self.ns.update(self.stored_nsvars)
 
-        # Handle any exception raised
+        # Containing code should handle any exception raised
         return False
