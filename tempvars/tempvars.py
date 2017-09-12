037e7bccec58ebc9b872c529aad90b4f84908db7
Brian Skinn <brian.skinn@gmail.com>
2017-09-11 23:53:57 -0400

    DEV: repr cleanup; Add storing tempvar values
     * `starts` and `ends` removed from `repr`
     * Line split for PEP8
     * Converted pop out of namespace from try-except to if
     * Added pop of created temp vars from namespace to post-with-suite
       storage dict
     * Fleshed out some comments

--- tempvars/tempvars.py
+++ tempvars/tempvars.py
@@ -20,10 +20,10 @@ import attr
 @attr.s()
 class TempVars(object):
 
-    # Arguments indicating variables to treat as temporary vars
+    ### Arguments indicating variables to treat as temporary vars
     tempvars = attr.ib(default=[])
-    starts = attr.ib(default=None)
-    ends = attr.ib(default=None)
+    starts = attr.ib(default=None, repr=False)
+    ends = attr.ib(default=None, repr=False)
 
     @tempvars.validator
     @starts.validator
@@ -52,11 +52,15 @@ class TempVars(object):
         # a list of strings
         raise te
 
-    # Flag for whether to restore the prior namespace contents
+    ### Flag for whether to restore the prior namespace contents
     restore = attr.ib(default=True, validator=attr.validators.instance_of(bool))
 
-    # Namespace for temp variable management. Defaults to the locals() of the
-    # scope at which the TempVars instance was created.
+    ### Namespace for temp variable management. Defaults to the locals() of the
+    # scope at which the TempVars instance was created. Python library docs
+    # frown at this, apparently:
+    #
+    #    https://docs.python.org/3/library/functions.html#locals
+    #
     # If a different namespace is desired for some reason, it can be passed here
     # Regardless, definitely don't want this in the `repr`, because it's a big
     # honking mess of stuff.
@@ -71,13 +75,16 @@ class TempVars(object):
 
     @ns.validator
     def ns_must_be_nsdict(self, _, val):
-        if not (type(val) == dict and all(map(lambda s: type(s) == str, val.keys()))):
+        if not (type(val) == dict and
+                all(map(lambda s: type(s) == str, val.keys()))):
             raise ValueError("'ns' must be a namespace dict")
 
     ### Internal vars, not set via the attrs __init__
     # Bucket for preserving variables temporarily removed from
     # the namespace
-    stored_nsvars = attr.ib(init=False, default={})
+    stored_nsvars = attr.ib(init=False, default={}, repr=False)
+    # Bucket for retaining the temporary variables after the context is exited
+    stored_tempvars = attr.ib(init=False, default={}, repr=False)
 
 
     def __enter__(self):
@@ -95,22 +102,20 @@ class TempVars(object):
                         self.tempvars.append(k)
 
         # Now that all of the variables have been identified,
-        #  store the values and cull them from the namespace
+        # pop any values that exist from the namespace and store
         for k in self.tempvars:
-            # Must account for the possibility that a temp var
-            # won't already exist.
-            try:
+            if k in self.ns:
                 self.stored_nsvars.update({k: self.ns.pop(k)})
-            except KeyError:
-                # No var found in namespace, do nothing
-                pass
 
         # Return instance so that users can inspect it if needed
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        # Clear the namespace of all temp variables, if they exist
-        list(self.ns.pop(_) for _ in self.tempvars if _ in self.ns)
+        # Pop any existing temp variables from the ns and into
+        # the storage dict
+        for k in self.tempvars:
+            if k in self.ns:
+                self.stored_tempvars.update({k: self.ns.pop(k)})
 
         # If restore is set, then repopulate the namespace with
         # the pre-existing values.  Otherwise, do nothing.
